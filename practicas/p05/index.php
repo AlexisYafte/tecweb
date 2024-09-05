0e44d80a03cc998bbe6860bee0f687f5408452eb
Yafte <yafte1epf@gmail.com>
2024-09-04 22:51:33 -0600

    Se agregó Ejercicio 3. Contenido de cada variable

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -30,5 +30,46 @@ echo "las variables \$b y \$c también adoptan este valor, ya que ambas son refe
 echo "<p>Una referencia en PHP significa que ambas variables (en este caso \$b y \$c) apuntan al mismo ";
 echo "espacio de memoria que la variable original (\$a), por lo que cualquier cambio en \$a se refleja automáticamente en \$b y \$c.</p>";
 
+// 3. Contenido de cada variable
+$a = "PHP5";
+echo "Valor de \$a después de la primera asignación: ";
+var_dump($a);
+echo "<br>";
+
+$z[] = &$a;
+echo "Contenido de \$z después de la asignación de referencia: ";
+var_dump($z); 
+echo "<br>";
+
+$b = "5a version de PHP";
+echo "Valor de \$b después de la tercera asignación: ";
+var_dump($b);
+echo "<br>";
+
+if (is_numeric($b)) {
+    $c = $b * 10;
+} else {
+    $c = 0; 
+    echo "No se puede multiplicar \$b porque no es numérico.<br>";
+}
+echo "Valor de \$c después de la cuarta asignación (multiplicación): ";
+var_dump($c);
+echo "<br>";
+
+$a .= $b;
+echo "Valor de \$a después de la concatenación con \$b: ";
+var_dump($a); 
+echo "<br>";
+
+$b *= $c;
+echo "Valor de \$b después de multiplicarse por \$c: ";
+var_dump($b);
+echo "<br>";
+
+$z[0] = "MySQL";
+echo "Contenido de \$z después de sobrescribir \$z[0]: ";
+var_dump($z); 
+echo "<br>";
+
 
 ?>
