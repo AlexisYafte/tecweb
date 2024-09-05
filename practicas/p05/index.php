d62f049a7f6c8d008a112d58d041fe2920e34b86
Yafte <yafte1epf@gmail.com>
2024-09-04 22:50:43 -0600

    Se agregó Ejercicio 2. Proporcionar valores

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -1,5 +1,5 @@
 <?php
-// Variables válidas
+// 1. Variables válidas
 $_myvar = "Válido porque comienza con un guion bajo";
 $_7var = "Válido porque comienza con un guion bajo";
 $myvar = "Válido porque empieza con un carácter válido";
@@ -9,5 +9,26 @@ echo $_myvar . "<br>";
 echo $_7var . "<br>";
 echo $myvar . "<br>";
 echo $var7 . "<br>";
-?>
 
+// 2. Proporcionar valores
+$a = "ManejadorSQL";
+$b = 'MySQL';
+$c = &$a; 
+
+echo '<br>';
+echo "a: $a, b: $b, c: $c<br>";
+
+// Segunda asignación
+$a = "PHP server";
+$b = &$a;
+
+echo "a: $a, b: $b, c: $c <br>";
+
+echo "<h3>Explicación:</h3>";
+echo "<p>En el segundo bloque de asignaciones, cuando cambiamos el valor de \$a a 'PHP server', ";
+echo "las variables \$b y \$c también adoptan este valor, ya que ambas son referencias a \$a.</p>";
+echo "<p>Una referencia en PHP significa que ambas variables (en este caso \$b y \$c) apuntan al mismo ";
+echo "espacio de memoria que la variable original (\$a), por lo que cualquier cambio en \$a se refleja automáticamente en \$b y \$c.</p>";
+
+
+?>
