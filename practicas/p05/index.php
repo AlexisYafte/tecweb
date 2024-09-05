3892efec4d10551caeaff27fabd5a9a680523f29
Yafte <yafte1epf@gmail.com>
2024-09-04 22:13:15 -0600

    Se agregó Ejercicio 1. Variables Validad

---
+++ practicas/p05/index.php
@@ -0,0 +1,13 @@
+<?php
+// Variables válidas
+$_myvar = "Válido porque comienza con un guion bajo";
+$_7var = "Válido porque comienza con un guion bajo";
+$myvar = "Válido porque empieza con un carácter válido";
+$var7 = "Válido porque los números pueden estar al final";
+
+echo $_myvar . "<br>";
+echo $_7var . "<br>";
+echo $myvar . "<br>";
+echo $var7 . "<br>";
+?>
+
